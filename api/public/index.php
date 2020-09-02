75767920398d47e486136fd221ac31776264e50a
nevadskiy <nevadskiy@gmail.com>
2020-09-02 22:56:34 +0300

    Add error handling

--- api/public/index.php
+++ api/public/index.php
@@ -6,9 +6,13 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$app->addErrorMiddleware((bool) getenv('APP_DEBUG'), true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
   $response->getBody()->write('{}');
