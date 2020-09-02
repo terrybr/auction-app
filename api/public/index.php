9578a81fd74b63e62d314f85e0b62deec52d59ff
nevadskiy <nevadskiy@gmail.com>
2020-09-02 23:51:09 +0300

    Extract container and dependencies

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
 /**
@@ -11,37 +10,14 @@ use Slim\Factory\AppFactory;
  */
 http_response_code(500);
 
-/**
- * Including autoloader.
- */
 require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * Set up app container.
- */
-$builder = new ContainerBuilder();
-
-$builder->addDefinitions([
-  'config' => [
-    'debug' => (bool) getenv('APP_DEBUG'),
-  ],
-]);
-
-$container = $builder->build();
+$container = require __DIR__.'/../config/container.php';
 
 $app = AppFactory::createFromContainer($container);
 
-/**
- * Add app middlewares.
- */
 (require __DIR__.'/../config/middleware.php')($app, $container);
 
-/**
- * Add app routes.
- */
 (require __DIR__.'/../config/routes.php')($app);
 
-/**
- * Run the app.
- */
 $app->run();
